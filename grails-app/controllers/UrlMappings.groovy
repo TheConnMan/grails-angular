79201cff6fbfef93e7ab11ec27e80bcd702ef8a8
TheConnMan <bcconn2112@gmail.com>
2015-11-07 16:44:07 -0500

    Convert UrlMappings to tabs

--- grails-app/controllers/UrlMappings.groovy
+++ grails-app/controllers/UrlMappings.groovy
@@ -1,14 +1,14 @@
 class UrlMappings {
 
-    static mappings = {
-        "/$controller/$action?/$id?(.$format)?"{
-            constraints {
-                // apply constraints here
-            }
-        }
+	static mappings = {
+		"/$controller/$action?/$id?(.$format)?"{
+			constraints {
+				// apply constraints here
+			}
+		}
 
-        "/"(controller:'toDo')
-        "500"(view:'/error')
-        "404"(view:'/notFound')
-    }
+		"/"(controller:'toDo')
+		"500"(view:'/error')
+		"404"(view:'/notFound')
+	}
 }
